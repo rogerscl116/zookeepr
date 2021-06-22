b37fef94f02645d9ddfecd57477e406e3816c234
charity.rogers <rogerscl116@gmail.com>
2021-06-22 17:57:41 -0500

    finish GET requests to serve page data

--- server.js
+++ server.js
@@ -116,8 +116,20 @@ app.post('/api/animals', (req, res) => {
     }
   });
 
-  app.get('/', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+  });
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+  });
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
   });
 
 app.listen(PORT, () => {
